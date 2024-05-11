6698afbbd0807dfe9818fb288dba109405437432
jagige <gigena_javier@hotmail.com>
2024-05-11 18:43:57 -0300

    consignas
    Se agregan los archivos necesarios para la tarea

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const $form = document.querySelector("#carta-a-santa");
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value
+const descripcionRegalo = $form["descripcion-regalo"].value
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
+
