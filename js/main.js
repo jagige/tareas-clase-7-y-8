c2f51ee06e3d2081edc4a0a5ae5e63ccceda6325
jagige <gigena_javier@hotmail.com>
2024-05-11 19:07:04 -0300

    Tarea clase 7
    El archivo main.js contiene las funciones de validación y el archivo pruebas.js  contiene las funciones de prueba

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ console.log(ciudad);
 console.log(comportamiento);
 console.log(descripcionRegalo);
 
+function validarNombre(nombre){
+    if (nombre.length === 0){
+        return "Este campo debe tener al menos 1 caracter";
+    }
+    if (nombre.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres";
+    }
+    return ""
+}
+
+function validarCiudad(ciudad){
+    if(ciudad === ""){
+        return "Este campo no debe decir: Selecciona...";
+    }
+    return ""
+}
+
+function validarDescripcionRegalo(descripcionRegalo){
+    if(descripcionRegalo.length === 0){
+        return "Este campo debe tener al menos 1 caracter";
+    }
+    return ""
+}
--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -6,3 +6,26 @@
 *
 * */
 
+//Esto es una copia. Los archivos funcionales son: pruebas.js y main.js de la carpeta js
+function probarValidarNombre(){
+    console.assert(validarNombre("") === "Este campo debe tener al menos 1 caracter",
+ "validar nombre no validó que el nombre no sea un string vacio");
+
+console.assert(
+    validarNombre("11111111112222222222333333333344444444445555555555") === "Este campo debe tener menos de 50 caracteres",
+"validar nombre no validó que el nombre sea menor a 50 caracteres");
+}
+
+function probarValidarCiudad(){
+    console.assert(validarCiudad("") === "Este campo no debe decir: Selecciona...",
+"validar ciudad no validó que no haya una ciudad vacia")
+}
+
+function probarValidarDescripcionRegalo(){
+    console.assert(validarDescripcionRegalo("") === "Este campo debe tener al menos 1 caracter", 
+"validar Descripcion Regalo no validó que el campo no esté vacío")
+}
+
+probarValidarNombre();
+probarValidarCiudad()
+probarValidarDescripcionRegalo()
