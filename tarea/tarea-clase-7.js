db9c379d9f13e7a2a03f2ab6f327146c80f48f60
jagige <gigena_javier@hotmail.com>
2024-05-12 13:10:44 -0300

    tarea clase 7
    se agregan las funciones al archivo de tarea para que todo quede en un solo archivo

--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -7,6 +7,45 @@
 * */
 
 //Esto es una copia. Los archivos funcionales son: pruebas.js y main.js de la carpeta js
+
+//dentro de main.js
+const $form = document.querySelector("#carta-a-santa");
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value
+const descripcionRegalo = $form["descripcion-regalo"].value
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
+
+function validarNombre(nombre){
+    if (nombre.length === 0){
+        return "Este campo debe tener al menos 1 caracter";
+    }
+    if (nombre.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres";
+    }
+    return ""
+}
+
+function validarCiudad(ciudad){
+    if(ciudad === ""){
+        return "Este campo no debe decir: Selecciona...";
+    }
+    return ""
+}
+
+function validarDescripcionRegalo(descripcionRegalo){
+    if(descripcionRegalo.length === 0){
+        return "Este campo debe tener al menos 1 caracter";
+    }
+    return ""
+}
+
+//dentro de pruebas.js
 function probarValidarNombre(){
     console.assert(validarNombre("") === "Este campo debe tener al menos 1 caracter",
  "validar nombre no validó que el nombre no sea un string vacio");
