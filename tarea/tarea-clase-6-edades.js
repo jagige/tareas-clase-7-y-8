b36fe1160417f1236867f5bd2b07938815e14a3d
jagige <gigena_javier@hotmail.com>
2024-06-20 11:47:22 -0300

    Finalizada la tarea edades
    cumple la consigna y se añaden los errores para los campos de edades

--- tarea/tarea-clase-6-edades.js
+++ tarea/tarea-clase-6-edades.js
@@ -16,33 +16,22 @@ TIP: Las edades no pueden tener decimales.
 const $edadIntegrantes = document.querySelector("#edadIntegrantes");
 
 function crearCampoEdades(numero) {
-  const familiar = document.createElement("label");
-  familiar.textContent = "Edad del familiar nº " + numero + ":";
+  const label = document.createElement("label");
+  label.textContent = "Edad del familiar nº " + numero + ":";
   const input = document.createElement("input");
   input.type = "number";
   input.setAttribute("class", "edadesFamilia");
   input.setAttribute("value", "0");
   input.setAttribute("min", "0");
   input.setAttribute("max", "150");
-  const br = document.createElement("br");
+  const div = document.createElement("div");
  
-  $edadIntegrantes.appendChild(familiar);
-  $edadIntegrantes.appendChild(input);
-  $edadIntegrantes.appendChild(br);
+  div.appendChild(label);
+  div.appendChild(input);
+  $edadIntegrantes.appendChild(div);
   return
 }
-/* Esto es lo que la función agrega en el html
-<label>Edad del integrante nº 1:</label>
-    <input type="number" class="edadesFamilia" value="2" min="1" max="150">
-    <br>
 
-////probar así
-     <div>
-            <label>Edad del integrante nº 1:</label>
-                <input type="number" class="edadesFamilia" value="2" min="1" max="150">
-                <span>error</span>
-        </div>
-*/
 const $advertencia = document.querySelector("#advertencia");
 const $resultados = document.querySelector("#resultados");
 const $grupoFamiliar = document.querySelector("#grupoFamiliar")
@@ -61,26 +50,26 @@ $botonEnviar.onclick = function () {
       $grupoFamiliar.className = "invalido"
        setTimeout(function(){
         $grupoFamiliar.className = ""
-     }, 400);
-///////////////////////tengo un error en 1.2 me muestra el error pero me agrega 2 campos
-    $advertencia.innerHTML = errorNumero;
-    $botonCalcular.className = "oculto";
-    $advertencia.className = "";
-  } else {
-    $botonCalcular.className = "";
-    $advertencia.className = "oculto";
-  }
+     }, 600);
 
-  if ($edadIntegrantes.innerHTML === "") {
-    for (let i = 0; i < $numeroGrupoFamiliar; i++) {
-      crearCampoEdades(i + 1);
-    }
+      $advertencia.innerHTML = errorNumero;
+      $botonCalcular.className = "oculto";
+      $advertencia.className = "";
   } else {
-    $edadIntegrantes.innerHTML = "";
-    for (let i = 0; i < $numeroGrupoFamiliar; i++) {
-      crearCampoEdades(i + 1);
-    }
-  }
+       $botonCalcular.className = "";
+       $advertencia.className = "oculto";
+      }
+
+   if ($edadIntegrantes.innerHTML === "") {
+      for (let i = 0; i < $numeroGrupoFamiliar; i++) {
+        crearCampoEdades(i + 1);
+      }
+    } else {
+        $edadIntegrantes.innerHTML = "";
+        for (let i = 0; i < $numeroGrupoFamiliar; i++) {
+         crearCampoEdades(i + 1);
+        }
+      }
 };
 
 
@@ -88,33 +77,9 @@ $botonBorrar.onclick = function () {
   $edadIntegrantes.innerHTML = "";
   $botonCalcular.className = "oculto";
   $resultados.className = "oculto";
-  document.querySelector("#advertencia").className = "oculto";
+  $advertencia.className = "oculto";
 };
 
-/*
-$botonCalcular.onclick = function () {
-  let $edadesFamilia = document.querySelectorAll(".edadesFamilia");
-  const arrayEdadesFamilia = [];
-  for (let i = 0; i < $edadesFamilia.length; i++) {
-    arrayEdadesFamilia.push(parseInt($edadesFamilia[i].value));
-    if ($edadesFamilia[i].value === "") {
-      arrayEdadesFamilia.pop();
-      arrayEdadesFamilia.push(0);
-    }
-  }
-
-
-  let acumulador = 0;
-  for (let i = 0; i < arrayEdadesFamilia.length; i++) {
-    acumulador += arrayEdadesFamilia[i];
-  }
-
-  $resultados.className = "";
-  document.querySelector("#mayorEdad").textContent = Math.max(...arrayEdadesFamilia);
-  document.querySelector("#menorEdad").textContent = Math.min(...arrayEdadesFamilia);
-  document.querySelector("#promedioEdad").textContent = acumulador / arrayEdadesFamilia.length;
-};
-*/
 
 $botonCalcular.onclick = function(){
   let $edadesFamilia = document.querySelectorAll(".edadesFamilia");
@@ -122,68 +87,25 @@ $botonCalcular.onclick = function(){
     if(validarEdad(i.value)){
       i.className = "invalido"
       i.value = "";
-      $advertencia.className = "";
+      $advertencia.innerHTML = "Solo se permiten números enteros positivos o cero. El resultado corresponde a los números válidos.";
+      $advertencia.className ="";
+      $botonCalcular.className = "oculto";
     } 
   });
   $edadesFamilia.forEach(function(i){
-    if(i.value ===""){
-    $edadIntegrantes.innerHTML = "";
-
-    } else{
-        const arrayEdadesFamilia = [];
-        for (let i = 0; i < $edadesFamilia.length; i++) {
-         arrayEdadesFamilia.push(parseInt($edadesFamilia[i].value));
-            if ($edadesFamilia[i].value === "") {
-             arrayEdadesFamilia.pop();
-             arrayEdadesFamilia.push(0);
-           }
+    if(i.value !=""){
+      const arrayEdadesFamilia = [];
+      for (let i = 0; i < $edadesFamilia.length; i++) {
+        arrayEdadesFamilia.push(parseInt($edadesFamilia[i].value));
+        if ($edadesFamilia[i].value === "") {
+          arrayEdadesFamilia.pop();   
+        }
   
-     
-      $resultados.className = "";
-      document.querySelector("#mayorEdad").textContent = Math.max(...arrayEdadesFamilia);
-     document.querySelector("#menorEdad").textContent = Math.min(...arrayEdadesFamilia);
-     document.querySelector("#promedioEdad").textContent = calcularPromedio(arrayEdadesFamilia) ;
-    }
+        $resultados.className = "";
+        document.querySelector("#mayorEdad").textContent = numeroMaximo(arrayEdadesFamilia);
+        document.querySelector("#menorEdad").textContent = numeroMinimo(arrayEdadesFamilia);
+        document.querySelector("#promedioEdad").textContent = calcularPromedio(arrayEdadesFamilia) ;
+      }
     }
   })
-
 }
-/*
-let errorEdad = validarNumero($edadesFamilia);
-for(let i =0; i < $edadesFamilia.length; i++){
-    if($edadesFamilia[i].value === ""){
-    $edadIntegrantes.innerHTML = "";
-    $botonCalcular.className = "oculto";
-    $advertencia.className = "";
-    }
-  
-}*/
-
-/*
-  if (errorEdad){
-    $edadIntegrantes.innerHTML = "";
-    $advertencia.innerHTML = errorEdad;
-    $botonCalcular.className = "oculto";
-    $advertencia.className = "";
-   } else {
-  const arrayEdadesFamilia = [];
-  for (let i = 0; i < $edadesFamilia.length; i++) {
-    arrayEdadesFamilia.push(parseInt($edadesFamilia[i].value));
-    if ($edadesFamilia[i].value === "") {
-      arrayEdadesFamilia.pop();
-      arrayEdadesFamilia.push(0);
-    }
-
-  let acumulador = 0;
-  for (let i = 0; i < arrayEdadesFamilia.length; i++) {
-    acumulador += arrayEdadesFamilia[i];
-  }
-
-  $resultados.className = "";
-  document.querySelector("#mayorEdad").textContent = Math.max(...arrayEdadesFamilia);
-  document.querySelector("#menorEdad").textContent = Math.min(...arrayEdadesFamilia);
-  document.querySelector("#promedioEdad").textContent = acumulador / arrayEdadesFamilia.length;
-   }
-  }
-}*/
-
