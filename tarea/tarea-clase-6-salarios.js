764edbd4cc957d0b74a0621d13b813052691609e
jagige <gigena_javier@hotmail.com>
2024-06-20 12:18:56 -0300

    comenzando
    Se agregan las consignas tarea clase 8, se incluyen archivos de validación y estilos, además se modifica el archivo CSS para un nombre general y usarlo en ambas tareas

--- tarea/tarea-clase-6-salarios.js
+++ tarea/tarea-clase-6-salarios.js
@@ -2,8 +2,13 @@
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
 Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
-
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+
+Tarea clase 8
+A las 2 tareas de la clase 6, ponerles las validaciones que consideren
+necesarias. (usando RegEx, Objetos, forEach, poner estilos,
+ mostrar los errores en la interfaz del usuario y escribir pruebas)
+TIP: Las edades no pueden tener decimales.
 */
 
 const $labelVacio = document.querySelector("#labelVacio");
